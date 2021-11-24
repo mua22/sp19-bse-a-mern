297fb423001d36012befe9de2881b131cbb79d7d
Usman Akram <usman.akram@gmail.com>
2021-11-23 22:18:00 -0800

    routing

--- admin/src/App.js
+++ admin/src/App.js
@@ -1,10 +1,32 @@
 import React from "react";
+import Login from "./components/views/auth/Login";
+import Register from "./components/views/auth/Register";
 import Products from "./products/Products";
-
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import "./App.css";
+import ProductDetails from "./products/ProductDetails";
 function App() {
   return (
     <div className="App">
-      <Products />
+      <Router>
+        <ul id="menu">
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/login">Login</Link>
+          </li>
+          <li>
+            <Link to="/register">Register</Link>
+          </li>
+        </ul>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/products/details/:id" element={<ProductDetails />} />
+          <Route path="/" element={<Products />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
